9973c0deb36331490efd80f3f4d5dc1a212df71d
Imran107 <imran.itim@gmail.com>
2022-02-13 15:06:12 -0500

    Inline implementation of the interface

--- src/io/javabrains/Greeter.java
+++ src/io/javabrains/Greeter.java
@@ -8,12 +8,21 @@ public class Greeter {
 
 	public static void main(String[] args) {
 		
-		// They both behaves exactly the same.
+		//Implementing interface through a class
 		Greeting helloWorldGreeting = new HelloWorldGreeting();
-		Greeting lambdaGreeting = () -> System.out.println("Hello World");
-		
 		helloWorldGreeting.perform();
+		
+		//Functional interface implementation with lambda function.
+		Greeting lambdaGreeting = () -> System.out.println("Hello World");
 		lambdaGreeting.perform();
+		
+		// This is the inline implementation of the interface without creating the Implementation class
+		Greeting greeting = new Greeting(){
+			public void perform(){
+				System.out.println("Hello World!");
+			}
+		};
+		greeting.perform();
 		
 	}
 }
